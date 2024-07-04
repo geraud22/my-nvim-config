7941c74133f70a6f745be8d6e46e076aa34017c7
Geraud WSL2 <geraud2201@gmail.com>
2024-07-04 13:55:45 +0200

    adding code completion snippets

--- lua/gopls-config.lua
+++ lua/gopls-config.lua
@@ -43,4 +43,14 @@ require('cmp').setup({
             require('luasnip').lsp_expand(args.body)
         end,
     },
+            mapping = {
+                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ['<C-e>'] = cmp.mapping.abort(),
+                ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-f>'] = cmp.mapping.scroll_docs(4),
+            },
+            sources = {
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' },
+            },
 })

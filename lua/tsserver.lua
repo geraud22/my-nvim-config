a23a25cb0c8a9d5e7e167e63d801441ed1f76d2f
Geraud WSL2 <geraud2201@gmail.com>
2024-10-22 11:52:04 +0200

    add vue and typescript development support

--- lua/tsserver.lua
+++ lua/tsserver.lua
@@ -1,5 +1,7 @@
 local lspconfig = require('lspconfig')
 local lsp_setup = require('my_lsp_config')
+local mason_registry = require('mason-registry')
+local vue_language_server_path  = "~/.local/share/nvim/mason/packages/vue-language-server/node_modules/@vue/language-server"
 
 lspconfig.ts_ls.setup{
 	on_attach = function(client, bufnr)
@@ -17,7 +19,40 @@ lspconfig.ts_ls.setup{
 				border = "single"
 			}
 		}, bufnr)
+
+		
 	end,
+	init_options = {
+        	plugins = {
+        		{
+        			name = '@vue/typescript-plugin',
+				location = vue_language_server_path,
+                		languages = { 'vue' },
+            		},
+        	},
+	},
 	capabilities = require('cmp_nvim_lsp').default_capabilities(),
 	filetypes = { "typescript", "typescriptreact", "typescript.tsx", "javascript", "javascriptreact", "javascript.jsx" }
+}
+
+lspconfig.volar.setup {
+	on_attach = function(client, bufnr)
+		lsp_setup.on_attach(client, bufnr)
+		if client.server_capabilities.documentFormattingProvider then
+            		vim.api.nvim_command [[augroup Format]]
+           		vim.api.nvim_command [[autocmd! * <buffer>]]
+            		vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
+            		vim.api.nvim_command [[augroup END]]
+        	end
+		
+		require('lsp_signature').on_attach({
+			bind = true,
+			handler_opts = {
+				border = "single"
+			}
+		}, bufnr)
+	end,
+
+	capabilities = require('cmp_nvim_lsp').default_capabilities(),
+	filetypes = { "vue" }
 }

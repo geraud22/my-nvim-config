0b32d2dbdb01e4bf715051e6b493650c20945fde
Geraud <geraud2201@gmail.com>
2025-11-13 11:25:07 +0200

    add some keybinds for crates.nvim

--- lua/geraud/lazy/rust-crates.lua
+++ lua/geraud/lazy/rust-crates.lua
@@ -3,7 +3,17 @@ return {
     tag = 'stable',
     event = { "BufRead Cargo.toml" },
     config = function()
-        require('crates').setup({
+        local crates = require("crates")
+        local opts = { silent = true }
+
+        vim.keymap.set("n", "<leader>ct", crates.toggle, opts)
+        vim.keymap.set("n", "<leader>cr", crates.reload, opts)
+        vim.keymap.set("n", "<leader>cv", crates.show_versions_popup, opts)
+        vim.keymap.set("n", "<leader>cf", crates.show_features_popup, opts)
+        vim.keymap.set("n", "<leader>cd", crates.show_dependencies_popup, opts)
+
+        crates.setup({
+            popup = { autofocus = true },
             completion = {
                 cmp = {
                     enabled = true,

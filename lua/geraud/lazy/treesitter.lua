3f7ac40c6d8c74e7a09bbdc714246930d3d58261
Geraud <geraud2201@gmail.com>
2026-04-21 11:11:29 +0200

    specify master branch for treesitter

--- lua/geraud/lazy/treesitter.lua
+++ lua/geraud/lazy/treesitter.lua
@@ -1,5 +1,8 @@
 return {
     'nvim-treesitter/nvim-treesitter',
+    branch = "master",
+    lazy = false,
+    build = ":TSUpdate",
     config = function()
        require("nvim-treesitter.configs").setup({
             ensure_installed = { "go", "gotmpl", "html", "javascript" },

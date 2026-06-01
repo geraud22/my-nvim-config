b4d5d94837ea174809aac5bc70b4f3262736ed60
Geraud <geraud2201@gmail.com>
2026-06-01 09:58:43 +0200

    leader l detaches lsp from the buffer

--- lua/geraud/remaps.lua
+++ lua/geraud/remaps.lua
@@ -4,3 +4,9 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<C-v>", '"+p')
 vim.keymap.set("v", "<C-c>", '"+y')
 vim.keymap.set("v", "<C-c>", '"+y')
+vim.keymap.set("n", "<leader>l", function()
+  for _, c in ipairs(vim.lsp.get_clients({ bufnr = 0 })) do
+    vim.lsp.buf_detach_client(0, c.id)
+    vim.notify("LSP clients detached from buffer", vim.log.levels.INFO)
+  end
+end, { desc = "Detach all LSP clients from current buffer" })
